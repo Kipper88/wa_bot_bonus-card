2e08f670e27da1c35dc76da9c195a189bc76f138
Kipper88 <153906933+Kipper88@users.noreply.github.com>
2024-10-13 15:07:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -19,23 +19,28 @@ app = Flask(__name__)
 @app.route('/', methods=['POST'])
 async def webhook():
     data = request.json
-    
-    if data['messageData']['typeMessage'] == 'textMessage':
-        chat_id = data['senderData']['chatId'].split('@')[0]
-        text = data['messageData']['textMessageData']['textMessage']
-        await handle_message(chat_id, text)
-        logging.info(f'Сообщение от {chat_id} успешно обработано')
-        
-    return "OK", 200
+
+    try:
+        if data['messageData']['typeMessage'] == 'textMessage':
+            chat_id = data['senderData']['chatId'].split('@')[0]
+            text = data['messageData']['textMessageData']['textMessage']
+            await handle_message(chat_id, text)
+            logging.info(f'Сообщение от {chat_id} успешно обработано')           
+        return "OK", 200
+    except Exception:
+        logging.error(f'Произошла ошибка при обработке сообщения от юзера {data['senderData']['chatId'].split('@')[0]}')
 
 async def checker_state():
     while True:
         for i in StateUser.birthday:
             if StateUser.birthday[i] < time.time - 1800: # 30 мин
                 del StateUser.birthday[i]
-        await asyncio.sleep(60)
+                logging.indo(f'Удалено ожидание ввода даты рождения от юзера {i}')
+            await asyncio.sleep(60)
 
 if __name__ == '__main__':
+    logging.info(f'Запуск сервера под именем {__name__}.')
     loop = asyncio.get_event_loop()
     loop.create_task(checker_state())
     app.run(port=9224)
+    logging.info(f'Сервер под именем {__name__} успешно запущен')
